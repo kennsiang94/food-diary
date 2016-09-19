5773f1eef1ff0e382f16328e5373697b5525d2d1
markjrobby <markjrobert@gmail.com>
2016-09-19 16:13:15 +0800

    add facebook settings configuration
    add facebook settings configuration for server side

--- food-diary/main.js
+++ food-diary/main.js
@@ -9,11 +9,38 @@ if(Meteor.isClient) {
 }
 
 if(Meteor.isServer) {
+	//Facebook configuration settings
+	ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+	});
+ 
+	ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1273586539339914',
+    secret: '67a494fc9cd1c8cbda26e75428307505'
+	});
 
 	
 }
 
 Meteor.methods({
-
+//Facebook login method calls
+Template.login.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
 	
 })
