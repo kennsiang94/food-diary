c56d1ed2cf189e82c8be39e7eae0a3db973cf241
kennsiang94 <halstzer@yahoo.com.sg>
2016-09-19 16:01:45 +0800

    fields for db

--- food-diary/main.js
+++ food-diary/main.js
@@ -3,6 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 MenuDishes = new Mongo.Collection('menuDishes');
 UserDishes = new Mongo.Collection('userDishes'); 
+//fields for menuDishes: dish name, picture link, description, price 
+//fields for userDishes: dish name, tried counter, want to try, favourite, comments
 
 if(Meteor.isClient) {
 
